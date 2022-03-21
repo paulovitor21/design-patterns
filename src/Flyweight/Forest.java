7bfbebc0820e123c3a55b447bdf912d9e3a07ef3
Paulo Vitor <80797702+paulovitor21@users.noreply.github.com>
2022-03-20 23:30:57 -0400

    design patterns renderizando uma floresta

---
+++ src/Flyweight/Forest.java
@@ -0,0 +1,26 @@
+package Flyweight;
+
+import Flyweight.Tree;
+import Flyweight.TreeFactory;
+import Flyweight.TreeType;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Forest extends JFrame {
+    private List<Tree> trees = new ArrayList<>();
+
+    public void plantTree(int x, int y, String name, Color color, String otherTreeData) {
+        TreeType type = TreeFactory.getTreeType(name, color, otherTreeData);
+        Tree tree = new Tree(x, y, type);
+        trees.add(tree);
+    }
+
+    public void paint(Graphics graphics) {
+        for (Tree tree : trees) {
+            tree.draw(graphics);
+        }
+    }
+}
